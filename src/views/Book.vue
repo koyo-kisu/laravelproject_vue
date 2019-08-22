1ed15319335a9c9446e32a334d746c738faed432
Koyo Kisu <koyokisu@gmail.com>
2019-08-22 14:56:50 +0900

    edit dist file

--- src/views/Book.vue
+++ src/views/Book.vue
@@ -58,7 +58,7 @@ export default {
         },
         filteredItems: function() {
             axios.post('/api/search', {
-                title: keyword
+                title: title
             }).then((res) => {
                 this.items = res.data
                 var texts = [];
