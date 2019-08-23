301f46091d2917273e5332c3e5e7bdb3332d1e8c
Koyo Kisu <koyokisu@gmail.com>
2019-08-23 14:55:37 +0900

    edit dist file

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">メインページ</router-link> |
+      <!-- <router-link to="/">メインページ</router-link> |
       <router-link to="/add">追加する</router-link> |
-      <router-link to="/show">詳細画面</router-link>
+      <router-link to="/show">詳細画面</router-link> -->
     </div>
     <router-view/>
   </div>
