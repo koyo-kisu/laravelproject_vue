35d75b5b9f00c077af23cf24cf222280480d9759
Koyo Kisu <koyokisu@gmail.com>
2019-08-25 19:24:34 +0900

    edit dist file

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,14 @@
 <template>
-    <div>
+    <div class="login_contents">
         <div>ログイン画面</div>
         <div>
             <div>ID</div>
             <div class="login_id"><input type="text" placeholder="ユーザーID"></div>
             <div>パスワード</div>
             <div class="login_pw"><input type="text" placeholder="パスワード"></div>
+        </div>
+        <div>
+            <input type="submit" value="ログイン">
         </div>
     </div>
 </template>
@@ -19,5 +22,7 @@ export default {
 </script>
 
 <style>
-
+.login_contents {
+    margin: 0 auto;
+}
 </style>
