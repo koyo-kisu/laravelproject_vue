08792d5bc5a669c9394082163acfc20f843e74f8
Koyo Kisu <koyokisu@gmail.com>
2019-09-01 15:53:32 +0900

    edit dist file

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -67,7 +67,7 @@ export default {
     methods: {
         //データを取得
         fetchTexts: function(task_id) {
-            axios.get('/api/edit', {
+            axios.post('/api/edit', {
                 id: task_id,
                 title: this.items.title,
                 author: this.items.author,
